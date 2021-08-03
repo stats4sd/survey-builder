a2167d1d5a2d1e891b35afc3cc3047da3ed8ca4b
Dave Mills <d.e.mills@stats4sd.org>
2021-08-03 17:16:44 +0100

    allow non-login users for demo:  - so we can link 'placeholder' users to projects to push via API

---
+++ app/Http/Requests/UserUpdateCrudRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserUpdateCrudRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // only allow updates if the user is logged in
+        return backpack_auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $id = $this->get('id') ?? request()->route('id');
+
+        return [
+            'email'    => 'nullable|unique:'.config('permission.table_names.users', 'users').',email,'.$id,
+            'name'     => 'nullable',
+            'password' => 'confirmed',
+        ];
+    }
+}
