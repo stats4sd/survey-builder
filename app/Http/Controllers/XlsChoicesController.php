97a80de1b7e68c7dc138fa90cfcf1863042933ff
Dave Mills <d.e.mills@stats4sd.org>
2022-03-31 17:10:22 +0100

    WIP - customisation

--- app/Http/Controllers/XlsChoicesController.php
+++ app/Http/Controllers/XlsChoicesController.php
@@ -11,7 +11,7 @@ class XlsChoicesController extends Controller
     {
         return ChoicesRow::with('ChoicesLabels')->get()
             ->map(function ($item) {
-                $item->choices_labels_by_lang = $item->choicesLabels ? $item->choiceLabels->groupBy('language_id') : null;
+                $item->choices_labels_by_lang = $item->choicesLabels ? $item->choicesLabels->groupBy('language_id') : null;
                 return $item;
             })
             ->groupBy('list_name')
